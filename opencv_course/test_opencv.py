a11af25ad0b2619249468cfbe3c25302fea5314a
diego7438 <danderson26@windwardschool.org>
2026-02-11 21:49:22 -0800

    Clean up repo: Remove everything except Uteach folder

--- opencv_course/test_opencv.py
+++
@@ -1,8 +0,0 @@
-import cv2
-import numpy as np
-import matplotlib
-import matplotlib.pyplot as plt
-
-print("OpenCV:", cv2.__version__)
-print("NumPy:", np.__version__)
-print("Matplotlib:", matplotlib.__version__)
