f171616b4e858a82d2bcee0b60261f7d4241b020
diego7438 <danderson26@windwardschool.org>
2026-02-11 21:28:12 -0800

    Update Fruit Ninja: fixed lag, added splash effects, and improved UI

---
+++ opencv_course/test_opencv.py
@@ -0,0 +1,8 @@
+import cv2
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
+print("OpenCV:", cv2.__version__)
+print("NumPy:", np.__version__)
+print("Matplotlib:", matplotlib.__version__)
